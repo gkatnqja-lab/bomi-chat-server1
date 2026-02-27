b52493ff5de3831b95fcf95e4176fc90322cb477
gkatnqja-lab <gkatnqja@gmail.com>
2026-02-26 21:09:09 -0800

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,5 +1,3 @@
 export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://www.shoppalacebeauty.com");
-  res.setHeader("Vary", "Origin");
   res.status(200).json({ ok: true });
 }
